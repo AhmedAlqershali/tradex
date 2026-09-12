b400aafdc231a35c3910f00268d221b78d70d1a1
ahmeddnael55-byte <ahmeddnael55@gmail.com>
2026-09-12 07:18:32 +0000

    Register Cloudinary filesystem provider

--- tradex-backend/bootstrap/providers.php
+++ tradex-backend/bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\RepositoryServiceProvider;
+use CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider;
 
 return [
     AppServiceProvider::class,
     RepositoryServiceProvider::class,
+    CloudinaryServiceProvider::class,
 ];
